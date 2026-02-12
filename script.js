82bced3fd2f9eb636b7c2d20efdb9c41500797be
Rihil09 <dlnrheal@gmail.com>
2026-02-12 13:18:34 +0800

    heloooooww

---
+++ script.js
@@ -0,0 +1,50 @@
+const wrapper = document.querySelector('.wrapper');
+const btnPopup = document.querySelector('.btnlogin-popup');
+const closeIcon = document.querySelector('.icon-close');
+const registerLink = document.querySelector('.register-link');
+const loginLink = document.querySelector('.login-link');
+const centerText = document.querySelector('.center-text');
+
+// Open login popup
+btnPopup.addEventListener('click', () => {
+    wrapper.classList.add('active-popup');
+    centerText.style.display = 'none';
+});
+
+// Switch to register form
+registerLink.addEventListener('click', () => {
+    wrapper.classList.add('active');
+});
+
+// Switch back to login form
+loginLink.addEventListener('click', () => {
+    wrapper.classList.remove('active');
+});
+
+// Close popup
+closeIcon.addEventListener('click', () => {
+    wrapper.classList.remove('active-popup');
+    wrapper.classList.remove('active');
+    centerText.style.display = 'block';
+});
+
+const controlButtons = document.querySelectorAll('.control-btn');
+const popups = document.querySelectorAll('.popup-box');
+const popupCloses = document.querySelectorAll('.popup-close');
+
+// Open popup
+controlButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const target = button.dataset.popup;
+
+        popups.forEach(popup => popup.classList.remove('active'));
+        document.getElementById(target).classList.add('active');
+    });
+});
+
+// Close popup
+popupCloses.forEach(close => {
+    close.addEventListener('click', () => {
+        popups.forEach(popup => popup.classList.remove('active'));
+    });
+});
